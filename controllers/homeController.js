5f3a3645f44f475c59e9ab84d74b0f954a5aac2f
DariaBezuhla <daria.freebird20@gmail.com>
2021-04-25 22:31:47 +0200

    Story_S6  overview of suggested sustainable alternatives on the home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,5 +2,29 @@
  * Render the index.ejs file (index page).
  */
 exports.getIndex = (req, res) => {
-        res.render("index");
+        let data = {
+                // apps: [
+                //         {
+                //                 name: "Good on you",
+                //                 description: "The Ultimate Guide to Sustainable Accessories",
+                //                 category: "Shopping"
+                //         },
+                //         {
+                //                 name: "Good on you",
+                //                 description: "The Ultimate Guide to Sustainable Accessories",
+                //                 category: "Shopping"
+                //         },
+                //         {
+                //                 name: "Good on you",
+                //                 description: "The Ultimate Guide to Sustainable Accessories",
+                //                 category: "Shopping"
+                //         },
+                // ],
+
+                appNameInCard: "Good on you!",
+                appDescriptionInCard: "The Ultimate Guide to Sustainable Accessories",
+                appCategoryBadge1: "Shopping",
+                appCategoryBadge2: "Food"
+        }
+        res.render("index", {data: data});
 };
