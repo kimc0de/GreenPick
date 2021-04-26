b1e11ff9e073f3a6b311702243d354a66f73b9df
Ly Nguyen <mly.nguyen@yahoo.de>
2021-04-26 22:40:02 +0200

    frontend for search field and category labels

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,29 +2,67 @@
  * Render the index.ejs file (index page).
  */
 exports.getIndex = (req, res) => {
-        let data = {
-                // apps: [
-                //         {
-                //                 name: "Good on you",
-                //                 description: "The Ultimate Guide to Sustainable Accessories",
-                //                 category: "Shopping"
-                //         },
-                //         {
-                //                 name: "Good on you",
-                //                 description: "The Ultimate Guide to Sustainable Accessories",
-                //                 category: "Shopping"
-                //         },
-                //         {
-                //                 name: "Good on you",
-                //                 description: "The Ultimate Guide to Sustainable Accessories",
-                //                 category: "Shopping"
-                //         },
-                // ],
+  // categories data - we may want to reuse this (then transfer to a separate file)
+  let categories = [
+    {
+      label: "Food",
+      className: "category-badge-food",
+      current: false
+    },
+    {
+      label: "Lifestyle",
+      className: "category-badge-lifestyle",
+      current: false
+    },
+    {
+      label: "Shopping",
+      className: "category-badge-shopping",
+      current: false
+    },
+    {
+      label: "Social",
+      className: "category-badge-social",
+      current: false
+    },
+    {
+      label: "Household",
+      className: "category-badge-household",
+      current: false
+    },
+    {
+      label: "Transport",
+      className: "category-badge-transport",
+      current: false
+    }
+  ]
 
-                appNameInCard: "Good on you!",
-                appDescriptionInCard: "The Ultimate Guide to Sustainable Accessories",
-                appCategoryBadge1: "Shopping",
-                appCategoryBadge2: "Food"
-        }
-        res.render("index", {data: data});
+  let data = {
+    // apps: [
+    //         {
+    //                 name: "Good on you",
+    //                 description: "The Ultimate Guide to Sustainable Accessories",
+    //                 category: "Shopping"
+    //         },
+    //         {
+    //                 name: "Good on you",
+    //                 description: "The Ultimate Guide to Sustainable Accessories",
+    //                 category: "Shopping"
+    //         },
+    //         {
+    //                 name: "Good on you",
+    //                 description: "The Ultimate Guide to Sustainable Accessories",
+    //                 category: "Shopping"
+    //         },
+    // ],
+
+    appNameInCard: "Good on you!",
+    appDescriptionInCard: "The Ultimate Guide to Sustainable Accessories",
+    appCategoryBadge1: "Shopping",
+    appCategoryBadge2: "Food"
+  }
+  res.render("index", {
+    categorySearch: false,
+    categories: categories,
+    data: data
+  });
 };
