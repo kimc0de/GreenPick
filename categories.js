1452f06080ae5cf52934cbb67191a2603a4c0af1
Ly Nguyen <38247392+lyrated@users.noreply.github.com>
2021-04-28 21:59:57 +0200

    Story 06 homepage suggested apps overview
    * Story_S6  overview of suggested sustainable alternatives on the home page
    
    * updated gitignore
    
    * frontend for search field and category labels
    
    * categories separated to a different file
    
    * fixed responsive design for medium screen sizes
    
    Co-authored-by: DariaBezuhla <daria.freebird20@gmail.com>
    Co-authored-by: Kim <61560491+kimc0de@users.noreply.github.com>

---
+++ categories.js
@@ -0,0 +1,26 @@
+exports.categories = [
+  {
+    label: "Food",
+    className: "category-badge-food"
+  },
+  {
+    label: "Lifestyle",
+    className: "category-badge-lifestyle"
+  },
+  {
+    label: "Shopping",
+    className: "category-badge-shopping"
+  },
+  {
+    label: "Social",
+    className: "category-badge-social"
+  },
+  {
+    label: "Household",
+    className: "category-badge-household"
+  },
+  {
+    label: "Transport",
+    className: "category-badge-transport"
+  }
+]
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,21 @@
+const { categories } = require('../categories');
+
 /**
  * Render the index.ejs file (index page).
  */
 exports.getIndex = (req, res) => {
-        res.render("index");
+  // to focus on one category, set activeCategory to the active label, else set to NULL
+  let activeCategory = null;
+
+  let data = {
+    appNameInCard: "Good on you!",
+    appDescriptionInCard: "The Ultimate Guide to Sustainable Accessories",
+    appCategoryBadge1: "Shopping",
+    appCategoryBadge2: "Food"
+  }
+  res.render("index", {
+    activeCategory: activeCategory,
+    categories: categories,
+    data: data
+  });
 };
