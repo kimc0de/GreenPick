29e96ab37d53150bc52930d91186b8b48753c0fd
kimc0de <kimngan.ledang@gmail.com>
2021-05-02 21:15:14 +0200

    index page and card takes data from database

--- controllers/greenpickAppController.js
+++ controllers/greenpickAppController.js
@@ -1,45 +1,44 @@
 const GreenpickApp = require("../models/greenpickApp");
 const { respondNoResourceFound } = require("./errorController");
 
-exports.renderNewApp = (req, res) => {
-  res.render("newApp");
-}
+module.exports = {
+  renderNewApp: (req, res) => {
+    res.render("newApp");
+  },
 
-exports.saveGreenpickApp = (req, res) => {
-  let newApp = new GreenpickApp({
-    category: req.body.category,
-    name: req.body.name,
-    website: req.body.website,
-    slogan: req.body.slogan,
-    description: req.body.description
-    //image: Buffer
-  });
-  newApp.save()
-    .then(() => {
-      res.render("newApp");
-    })
-    .catch(error => {
-      res.send(error);
+  saveGreenpickApp: (req, res) => {
+    let newApp = new GreenpickApp({
+      category: req.body.category,
+      name: req.body.name,
+      website: req.body.website,
+      slogan: req.body.slogan,
+      description: req.body.description
+      //image: Buffer
     });
-};
-
-
-/**
- * Green Pick app details page
- */
-
-exports.getDetailsPage = async (req, res) => {
-  let id = req.params.id;
-  try {
-    const app = await GreenpickApp.findById(id);
-    res.render('./detailsPage',
-      {
-        id: id,
-        app: app,
-        appImg: '/images/tooGoodToGoLogo.png' // will be replaced when we implement images
+    newApp.save()
+      .then(() => {
+        res.render("newApp");
+      })
+      .catch(error => {
+        res.send(error);
       });
-  } catch (error) {
-    console.error(error);
-    respondNoResourceFound(req, res);
+  },
+  /**
+   * Green Pick app details page
+   */
+  getDetailsPage: async (req, res) => {
+    let id = req.params.id;
+    try {
+      const app = await GreenpickApp.findById(id);
+      res.render('./detailsPage',
+        {
+          id: id,
+          app: app,
+          appImg: '/images/tooGoodToGoLogo.png' // will be replaced when we implement images
+        });
+    } catch (error) {
+      console.error(error);
+      respondNoResourceFound(req, res);
+    }
   }
-};
+}
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,21 +1,32 @@
 const { categories } = require('../categories');
+const GreenpickApp = require("../models/greenpickApp");
 
 /**
  * Render the index.ejs file (index page).
  */
-exports.getIndex = (req, res) => {
-  // to focus on one category, set activeCategory to the active label, else set to NULL
-  let activeCategory = null;
+module.exports = {
+  /**
+   * Get all app from data base
+   * TODO: limit the apps show on index page
+   */
+  getAllApps: (req, res, next) => {
+    GreenpickApp.find((error, apps) => {
+      if(error) {
+        res.render("error");
+      }
+      req.data = apps;
+      next();
+    })
+  },
 
-  let data = {
-    appNameInCard: "Good on you!",
-    appDescriptionInCard: "The Ultimate Guide to Sustainable Accessories",
-    appCategoryBadge1: "Shopping",
-    appCategoryBadge2: "Food"
+  renderIndex: (req, res) => {
+    // to focus on one category, set activeCategory to the active label, else set to NULL
+    let activeCategory = null;
+    res.render("index", {
+      activeCategory: activeCategory,
+      categories: categories,
+      data: req.data
+    });
   }
-  res.render("index", {
-    activeCategory: activeCategory,
-    categories: categories,
-    data: data
-  });
-};
+}
+
