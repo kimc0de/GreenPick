fcf1a20005dcdd8733d5847567d6bf431b4a88ef
Ly Nguyen <mly.nguyen@yahoo.de>
2021-05-02 16:29:40 +0200

    error handling + tabsize configuration

--- controllers/greenpickAppController.js
+++ controllers/greenpickAppController.js
@@ -1,40 +1,45 @@
 const GreenpickApp = require("../models/greenpickApp");
+const { respondNoResourceFound } = require("./errorController");
 
 exports.renderNewApp = (req, res) => {
-    res.render("newApp");
+  res.render("newApp");
 }
 
-
 exports.saveGreenpickApp = (req, res) => {
-    let newApp = new GreenpickApp ({
-        category: req.body.category,
-        name: req.body.name,
-        website: req.body.website,
-        slogan: req.body.slogan,
-        description: req.body.description
-        //image: Buffer
+  let newApp = new GreenpickApp({
+    category: req.body.category,
+    name: req.body.name,
+    website: req.body.website,
+    slogan: req.body.slogan,
+    description: req.body.description
+    //image: Buffer
+  });
+  newApp.save()
+    .then(() => {
+      res.render("newApp");
+    })
+    .catch(error => {
+      res.send(error);
     });
-    newApp.save()
-        .then(() => {
-            res.render("newApp");
-        })
-        .catch(error => {
-            res.send(error);
-        });
 };
+
 
 /**
  * Green Pick app details page
  */
 
 exports.getDetailsPage = async (req, res) => {
-    let id = req.params.id;
+  let id = req.params.id;
+  try {
     const app = await GreenpickApp.findById(id);
-
     res.render('./detailsPage',
-        {
-            id: id,
-            app: app,
-            appImg: '/images/tooGoodToGoLogo.png' // will be replaced when we implement images
-        });
+      {
+        id: id,
+        app: app,
+        appImg: '/images/tooGoodToGoLogo.png' // will be replaced when we implement images
+      });
+  } catch (error) {
+    console.error(error);
+    respondNoResourceFound(req, res);
+  }
 };
