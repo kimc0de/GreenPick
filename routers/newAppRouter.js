28db1afdb999280f57dfe1440329ae5b9544083c
DariaBezuhla <daria.freebird20@gmail.com>
2021-05-07 09:28:07 +0200

    *cleaned up the drop down menu *renamed greenPickAppController to newAppController

--- routers/newAppRouter.js
+++ routers/newAppRouter.js
@@ -1,11 +1,11 @@
-const greenPickAppController = require('../controllers/newAppController');
+const newAppController = require('../controllers/newAppController');
 const router = require('express').Router();
 
 // routes to add new app page
-router.get("/add", greenPickAppController.renderNewApp);
-router.post("/add", greenPickAppController.saveGreenPickApp);
+router.get("/add", newAppController.renderNewApp);
+router.post("/add", newAppController.saveGreenPickApp);
 
 // route to details page
-router.get("/app/:id", greenPickAppController.getDetailsPage);
+router.get("/app/:id", newAppController.getDetailsPage);
 
 module.exports = router;
