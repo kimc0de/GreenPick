3bec183c7faafc59e3f472b0ec43fc75863af853
DariaBezuhla <daria.freebird20@gmail.com>
2021-05-07 13:40:30 +0200

    added required to the input fields

--- routers/newAppRouter.js
+++ routers/newAppRouter.js
@@ -1,11 +1,11 @@
-const newAppController = require('../controllers/newAppController');
+const greenPickAppController = require('../controllers/greenPickAppController');
 const router = require('express').Router();
 
 // routes to add new app page
-router.get("/add", newAppController.renderNewApp);
-router.post("/add", newAppController.saveGreenPickApp);
+router.get("/add", greenPickAppController.renderNewApp);
+router.post("/add", greenPickAppController.saveGreenPickApp);
 
 // route to details page
-router.get("/app/:id", newAppController.getDetailsPage);
+router.get("/app/:id", greenPickAppController.getDetailsPage);
 
 module.exports = router;
