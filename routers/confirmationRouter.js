eb0a7198a7d3030ca464c986153f971ffd28ee25
kimc0de <kimngan.ledang@gmail.com>
2021-05-08 19:53:16 +0200

    error, confirmation css

--- routers/confirmationRouter.js
+++
@@ -1,7 +0,0 @@
-const homeController = require('../controllers/homeController');
-const router = require('express').Router();
-
-//route to confirmation page
-router.get("/confirmation", homeController.getConfirmation);
-
-module.exports = router;
