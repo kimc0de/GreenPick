e2aad898cd406b6421a69d2b8b23902c581f7368
DariaBezuhla <daria.freebird20@gmail.com>
2021-05-12 18:16:15 +0200

    added heroku and enviroment variable to app

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb://localhost:27017/green_pick",
-  {useNewUrlParser: true}
+mongoose.connect(process.env.MONGODB_URI ||
+    "mongodb://localhost:27017/green_pick",
+    {useNewUrlParser: true}
 );
 const db = mongoose.connection;
 
