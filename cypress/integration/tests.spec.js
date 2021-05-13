9ff9f6638ede207f270038ceec3f7aebeea11adb
Stefi <stefiimbuzan@yahoo.com>
2021-05-13 15:34:59 +0200

    * Setup cypress for CI

---
+++ cypress/integration/tests.spec.js
@@ -0,0 +1,16 @@
+describe('Smoke Tests', () => {
+  it('Landing page opens', () => {
+    cy.visit('/');
+  });
+
+  it('Add alternative page opens', () => {
+    cy.visit('/');
+    cy.get('.nav-item').contains('Add alternative').click();
+    cy.url().should('contain', '/add');
+  });
+
+  it('Logo redirects to landing page', () => {
+    cy.get('#brand-logo').click();
+    cy.url().should('contain', '/');
+  });
+});
