3f2930179e1e80460924f6e08c68f8c3bc5aed49
kimc0de <kimngan.ledang@gmail.com>
2021-05-22 18:48:08 +0200

    user model association

--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose"),
-    userSchema = mongoose.Schema({
-        username: {type: String, required: true},
-        email: {type: String, required: true, unique: true, lowercase: true},
-        password: {type: String, required: true}
-    });
+  userSchema = mongoose.Schema({
+    username: {type: String, required: true},
+    email: {type: String, required: true, unique: true, lowercase: true},
+    password: {type: String, required: true},
+    apps: [{type: mongoose.Schema.Types.ObjectId, ref: "greenPickApp"}]
+  });
 
 module.exports = mongoose.model("users", userSchema);
