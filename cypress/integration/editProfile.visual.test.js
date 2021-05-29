28aa1a595d0c06ca2f0b57296b8fe61aa95cc6cb
kimc0de <kimngan.ledang@gmail.com>
2021-05-29 16:33:57 +0200

    Fix cypress edit-profile visual test again

--- cypress/integration/editProfile.visual.test.js
+++ cypress/integration/editProfile.visual.test.js
@@ -1,11 +1,8 @@
 describe('Edit profile page', () => {
-  before(() => {
-    cy.visit('/user/60af9bdd5e252e00040c5d3e/edit'); //this is real user on our mongo atlas database
-   // cy.visit('localhost:3000/user/60aad19ca4a7f6434225c7d5/edit');
-  });
 
   it('shows label and input in one line when viewport is greater than 576px', () => {
-    cy.viewport('macbook-13');
+    cy.viewport(1280, 800);
+    cy.visit('/user/60af9bdd5e252e00040c5d3e/edit');
     cy.get('#edit-profile-form').should('be.visible');
     cy.get('#edit-username').should('be.visible');
     cy.get('#edit-email').should('be.visible');
@@ -14,7 +11,8 @@ describe('Edit profile page', () => {
   });
 
   it('shows label and input in two line when viewport is smaller than 576px', () => {
-    cy.viewport('iphone-x');
+    cy.viewport(375, 812);
+    cy.visit('/user/60af9bdd5e252e00040c5d3e/edit');
     cy.get('#edit-profile-form').should('be.visible');
     cy.get('#edit-username').should('be.visible');
     cy.get('#edit-email').should('be.visible');
