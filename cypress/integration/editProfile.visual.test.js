7c326818e0548ecf399d1717e21207b08f06d08a
kimc0de <kimngan.ledang@gmail.com>
2021-05-29 13:00:29 +0200

    Fix cypress edit-profile visual test

--- cypress/integration/editProfile.visual.test.js
+++ cypress/integration/editProfile.visual.test.js
@@ -1,7 +1,7 @@
 describe('Edit profile page', () => {
   before(() => {
-    //cy.visit('/user/60af9bdd5e252e00040c5d3e/edit'); //this is real user on our mongo atlas database
-    cy.visit('localhost:3000/user/60aad19ca4a7f6434225c7d5/edit');
+    cy.visit('/user/60af9bdd5e252e00040c5d3e/edit'); //this is real user on our mongo atlas database
+   // cy.visit('localhost:3000/user/60aad19ca4a7f6434225c7d5/edit');
   });
 
   it('shows label and input in one line when viewport is greater than 576px', () => {
