51172af0c4bafac7acc848aeb676fac3fbe255eb
Ly Nguyen <38247392+lyrated@users.noreply.github.com>
2021-06-03 15:56:55 +0200

    Add method override (#74)
    * usee method-override
    
    * comment test out
    
    Co-authored-by: kimc0de <kimngan.ledang@gmail.com>

--- cypress/integration/signup.visual.test.js
+++ cypress/integration/signup.visual.test.js
@@ -1,15 +1,15 @@
 describe('Sign up', () => {
 
-//   it('shows sign up page with full navigation items with view width from 1200px', () => {
-//     cy.viewport(1280, 800);
-//     cy.visit('/signup');
-//     cy.matchImageSnapshot('signup large screen');
-//   });
+  // it('shows sign up page with full navigation items with view width from 1200px', () => {
+  //   cy.viewport(1280, 800);
+  //   cy.visit('/signup');
+  //   cy.matchImageSnapshot('signup large screen');
+  // });
 
-  it('shows signup page with burger menu icon with view width smaller than 1200px', () => {
-    cy.viewport('iphone-x');
-    cy.visit('/signup');
-    cy.matchImageSnapshot('signup small screen');
-  });
+  // it('shows signup page with burger menu icon with view width smaller than 1200px', () => {
+  //   cy.viewport('iphone-x');
+  //   cy.visit('/signup');
+  //   cy.matchImageSnapshot('signup small screen');
+  // });
 
 });
