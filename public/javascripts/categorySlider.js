cbf000ac7e0b5813b6569e0bd3b9a828d7a31536
kimc0de <kimngan.ledang@gmail.com>
2021-06-08 20:48:26 +0200

    AJAX for homepage apps

--- public/javascripts/categorySlider.js
+++ public/javascripts/categorySlider.js
@@ -1,15 +1,46 @@
 $(document).ready(() => {
-  $("#category-slider button").each(() => {
-    let variable = this;
-    $(this).on("click", () => {
+  $("#category-slider button").click(function() {
+    let categoryName = $(this).attr('name');
+    $.get("/api/category/" + categoryName + "?format=json", (data) => {
       $("#homepage-apps").html('');
-      console.log("This is " , variable);
-      let category = this.getAttribute("name");
-
-      $.get("/api/category/" + category, (data) => {
-        console.log(data);
-
-      }).then(() => {});
-    });
+        let apps = data.data.apps;
+        if (apps.length == 0) {
+          $("#homepage-apps").append(
+            `<h3> There is no green pick application for this category </h3>`
+          );
+        }
+        apps.forEach((app) => {
+        $("#homepage-apps").append(
+          `
+          <div class="row" id="app-card">
+              <div class="col col-lg-4 col-md-6 col-12" onclick="location.href='/app/${app._id}';">
+                <div class="card-body category-${categoryName} d-flex">
+                  <img class="app-logo" src="/images/brandlogos/goodOnYou.png">
+                  <div>
+                    <h2>${app.name}</h2>
+                    <p class="app-slogan">${app.slogan}</p>
+                    <div class="badge rounded-pill text-light category-badge-${categoryName}">${categoryName}</div>
+                  </div>
+                </div>
+              </div>
+          </div>
+          `
+        );
+      })
+    })
   });
 });
+
+// let getCategoryById = (categoryId) => {
+//     let result;
+//     categories.forEach(category => {
+//       if(category._id.equals(categoryId)) {
+//         result = category;
+//       }
+//     });
+//     return result;
+//   }
+// data.forEach(app => {
+//   let category = getCategoryById(app.category);
+//
+// }
