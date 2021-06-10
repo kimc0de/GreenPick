c18986d5863a3933b1a533b34bf124ed316c0669
Ly Nguyen <mly.nguyen@yahoo.de>
2021-06-10 18:56:05 +0200

    Frontend changes with badges

--- public/javascripts/categorySlider.js
+++ public/javascripts/categorySlider.js
@@ -1,8 +1,16 @@
 $(document).ready(() => {
-  $("#category-slider button").click(function() {
-    let categoryName = $(this).attr('name');
-    $.get("/api/category/" + categoryName + "?format=json", (data) => {
-      $("#homepage-apps").html('');
+  const homeContent = $("#homepage-apps").html();
+  console.log(homeContent);
+
+  $("#category-slider button").click(function () {
+    $("#category-slider button").addClass("inactive");
+    $(this).removeClass("inactive");
+
+    if (!$(this).hasClass("selected")) {
+      $(this).addClass("selected");
+      let categoryName = $(this).attr('name');
+      $.get("/api/category/" + categoryName + "?format=json", (data) => {
+        $("#homepage-apps").html('');
         let apps = data.data.apps;
         if (apps.length == 0) {
           $("#homepage-apps").append(
@@ -10,23 +18,28 @@ $(document).ready(() => {
           );
         }
         apps.forEach((app) => {
-        $("#homepage-apps").append(
-          `
-          <div class="row" id="app-card">
-              <div class="col col-lg-4 col-md-6 col-12" onclick="location.href='/app/${app._id}';">
-                <div class="card-body category-${categoryName} d-flex">
-                  <img class="app-logo" src="/images/brandlogos/goodOnYou.png">
-                  <div>
-                    <h2>${app.name}</h2>
-                    <p class="app-slogan">${app.slogan}</p>
-                    <div class="badge rounded-pill text-light category-badge-${categoryName}">${categoryName}</div>
-                  </div>
+          $("#homepage-apps").append('<div class="row" id="app-card"></div>');
+          $("#app-card").append(
+            `
+            <div class="col col-lg-4 col-md-6 col-12" onclick="location.href='/app/${app._id}';">
+              <div class="card-body category-${categoryName} d-flex">
+                <img class="app-logo" src="/images/brandlogos/goodOnYou.png">
+                <div>
+                  <h2>${app.name}</h2>
+                  <p class="app-slogan">${app.slogan}</p>
+                  <div class="badge rounded-pill text-light category-badge-${categoryName}">${categoryName}</div>
                 </div>
               </div>
-          </div>
+            </div>
           `
-        );
-      })
-    })
+          );
+        });
+      });
+    } else {
+      $(this).removeClass("selected");
+      $("#category-slider button").removeClass("inactive");
+      $("#homepage-apps").html("");
+      $("#homepage-apps").append(homeContent);
+    }
   });
 });
