97294d4e7dae85a6471f8bee0134d18c4cc2c942
Kim <61560491+kimc0de@users.noreply.github.com>
2021-07-03 14:24:12 +0200

    Add google authentication (#98)
    * google-auth
    
    * finish styling
    
    * fix link to details page

--- models/user.js
+++ models/user.js
@@ -1,15 +1,18 @@
 const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require('mongoose-findorcreate');
 
 const mongoose = require("mongoose"),
   userSchema = mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
+    username: { type: String, unique: true },
+    email: { type: String, unique: true, lowercase: true },
     apps: [{ type: mongoose.Schema.Types.ObjectId, ref: "greenPickApp" }],
-    favApps: [{ type: mongoose.Schema.Types.ObjectId, ref: "greenPickApp" }]
+    favApps: [{ type: mongoose.Schema.Types.ObjectId, ref: "greenPickApp" }],
+    googleId: {type: String}
   });
 
 userSchema.plugin(passportLocalMongoose, {
   usernameField: "username"
 });
+userSchema.plugin(findOrCreate);
 
 module.exports = mongoose.model("users", userSchema);
