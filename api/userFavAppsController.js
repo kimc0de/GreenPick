7797c682b03ddfa734fde32cfe8160ced1622d95
kimc0de <kimngan.ledang@gmail.com>
2021-07-14 21:45:22 +0200

    fix favorites api

--- api/userFavAppsController.js
+++ api/userFavAppsController.js
@@ -3,28 +3,28 @@ const {respondNoResourceFound, redirectIfUnauthorized} = require("../controllers
 const GreenPickApp = require("../models/greenPickApp");
 
 module.exports = {
-  
+
   getFavouriteAppsByUser: async (req, res, next) => {
     try {
       if (!req.user) {
         redirectIfUnauthorized(req, res)
       } else {
         let favApps = req.user.favApps;
-        res.locals = await GreenPickApp.find({apps: favApps._id});
-        next()
+        res.locals = await GreenPickApp.find({_id: favApps});
+        next();
       }
     } catch (error) {
       console.log(error);
       respondNoResourceFound(req, res);
     }
   },
-  
-  
+
+
   respondJSON: (req, res) => {
     res.json({
       status: httpStatus.OK,
       data: res.locals
     });
   },
-  
+
 }
